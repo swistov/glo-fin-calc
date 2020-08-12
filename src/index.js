2c34ca2403ffb3413243f7296888ed01aa8cfb35
nsvistov <swistov@gmail.com>
2020-08-12 23:12:19 +0300

    ДЗ 1

--- src/index.js
+++ src/index.js
@@ -3,24 +3,9 @@ import ReactDOM from 'react-dom';
 import App from "./App";
 
 
-const Header = ({ text }) => <h1 id='hi'>{text}</h1>
-
-const Element = <p>Параграф</p>
-
-class Main extends React.Component {
-    render() {
-        return (
-            <div className='main'>
-                <Header text={'Текст'} />
-                {Element}
-            </div>
-        )
-    }
-}
-
 ReactDOM.render(
   <React.StrictMode>
-    <Main />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
