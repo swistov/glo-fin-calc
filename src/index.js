4c7e55f1865b2cc6d26b565f5d6979ec2736fb3d
nsvistov <swistov@gmail.com>
2020-08-12 21:01:06 +0300

    Начало

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {render} from "@testing-library/react";
+
+const FunctionComponents = () =>
+    React.createElement('h1', null, 'Заголовки');
+
+const Element = React.createElement('p', null, 'Paragraph');
+
+class ClassComponents extends Component{
+    render() {
+        return React.createElement('div', {className: 'main'}, React.createElement(FunctionComponents), Element)
+    }
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ClassComponents />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
